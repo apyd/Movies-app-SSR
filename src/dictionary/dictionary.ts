b74b57eb25751e1fc4512cfa64daca5806cd6a69
apyd <arturpydych@gmail.com>
2022-08-03 22:22:47 +0200

    added missing dictionary

---
+++ src/dictionary/dictionary.ts
@@ -0,0 +1,52 @@
+export const SORT_OPTIONS = [
+  {
+    id: 1,
+    label: "Rating",
+    value: "average_vote",
+  },
+  {
+    id: 2,
+    label: "Release date",
+    value: "release_date",
+  },
+];
+
+export const FILTER_OPTIONS = [
+  {
+    id: '1',
+    name: 'genre',
+    label: 'All',
+    value: ''
+  },
+  {
+    id: '2',
+    name: 'genre',
+    label: 'Documentary',
+    value: 'documentary'
+  },
+  {
+    id: '3',
+    name: 'genre',
+    label: 'Comedy',
+    value: 'comedy'
+  },
+  {
+    id: '4',
+    name: 'genre',
+    label: 'Horror',
+    value: 'horror'
+  },
+  {
+    id: '5',
+    name: 'genre',
+    label: 'Crime',
+    value: 'crime'
+  }
+];
+
+export const GENRES = [
+  { id: '1', title: 'Documentary' },
+  { id: '2', title: 'Comedy' },
+  { id: '3', title: 'Horror' },
+  { id: '4', title: 'Crime' }
+];
